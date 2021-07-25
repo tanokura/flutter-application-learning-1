0e76af0546ca4f5af19f400811712170de840860
ryo_tanokura <flying-ryo@hotmail.co.jp>
2021-07-25 20:18:57 +0900

    Add Pin Image

--- lib/ui/guide/map_guide_page.dart
+++ lib/ui/guide/map_guide_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:math';
 
 class MapGuidePage extends HookWidget {
   late PhotoViewController photoViewController;
@@ -26,17 +28,33 @@ class Map extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    late PhotoViewController photoViewController;
-    var preScale = 0.0;
+    // late PhotoViewController photoViewController;
+    final PhotoViewController photoViewController = PhotoViewController();
+    final Size size = MediaQuery.of(context).size;
+    final sizeTop = size.height / 2;
+    final sizeLeft = size.width / 2;
+
+    var pinTop;
+    var pinLeft;
+    var pinTop2;
+    var pinLeft2;
+
+    final relativeTop = -260;
+    final relativeleft = 35;
+    final relativeTop2 = 210;
+    final relativeleft2 = -131;
+
+    var preScale = 0.00;
 
     useEffect(() {
       print('### useEffect init');
       // TODO: initalize
-      photoViewController = PhotoViewController();
+      // photoViewController = PhotoViewController();
       return () {
-        photoViewController.dispose();
+        // photoViewController.dispose();
       };
     }, const []);
+    print('size(height:${size.height}, width:${size.width})');
 
     return Stack(children: <Widget>[
       PhotoView(
@@ -48,43 +66,63 @@ class Map extends HookWidget {
           color: Theme.of(context).canvasColor,
         ),
       ),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          HookBuilder(
-            // Listening on the PhotoView's controller stream
-            builder: (context) {
-              final stream = useStream(photoViewController.outputStateStream);
-              final currentScale = stream.data?.scale;
-              if (currentScale != null) {
-                if ((preScale - currentScale).abs() > 0.1) {
-                  preScale = double.parse(currentScale.toStringAsFixed(1));
-                  onChangeScale(preScale);
-                }
-              }
-              // return Center(
-              //   child: Text(
-              //     'Scale compared to the original: \n${stream.data?.scale}',
-              //     textAlign: TextAlign.center,
-              //     style: TextStyle(fontSize: 20),
-              //   ),
-              // );
-              return Text('');
-            },
-          ),
-          // ElevatedButton(
-          //   onPressed: () {
-          //     // photoViewController.scale = photoViewController.initial.scale;
-          //     final tmpSacle = photoViewController.initial.scale;
-          //     if (tmpSacle!= null) {
-          //       scale.value = tmpSacle;
-          //     }
+      HookBuilder(
+          // Listening on the PhotoView's controller stream
+          builder: (context) {
+        final stream = useStream(photoViewController.outputStateStream);
+        final currentScale = stream.data?.scale;
+        final position = stream.data?.position;
+        if (currentScale != null && position != null) {
+          if ((preScale - currentScale).abs() > 0.01) {
+            preScale = double.parse(currentScale.toStringAsFixed(2));
+            onChangeScale(preScale);
+          }
 
-          //   },
-          //   child: Text('Reset Scale'),
-          // ),
-        ],
-      )
+          pinTop = sizeTop + (relativeTop + position.dy) * sqrt(currentScale);
+          pinLeft =
+              sizeLeft + (relativeleft + position.dx) * sqrt(currentScale);
+          print(position);
+          print('pin(sizeTop:$sizeTop, sizeLeft:$sizeLeft)');
+          print('pin(top:$pinTop, left:$pinLeft)');
+
+          // TODO: スケール計算
+          pinTop2 = sizeTop + (relativeTop2 + position.dy) * sqrt(currentScale);
+          pinLeft2 =
+              sizeLeft + (relativeleft2 + position.dx) * sqrt(currentScale);
+        }
+        return Stack(children: <Widget>[
+          Positioned(
+              left: pinLeft,
+              top: pinTop,
+              child: IconButton(
+                  // SvgPictureを指定できる
+                  icon: SvgPicture.asset(
+                    'assets/icon/pin-fill.svg',
+                    // color: Colors.blue,
+                    semanticsLabel: 'pin',
+                    width: 50,
+                    height: 50,
+                  ),
+                  onPressed: () {
+                    print('## pin press1!!');
+                  })),
+          Positioned(
+              left: pinLeft2,
+              top: pinTop2,
+              child: IconButton(
+                  // SvgPictureを指定できる
+                  icon: SvgPicture.asset(
+                    'assets/icon/pin-fill.svg',
+                    // color: Colors.blue,
+                    semanticsLabel: 'pin',
+                    width: 50,
+                    height: 50,
+                  ),
+                  onPressed: () {
+                    print('## pin press2!!');
+                  }))
+        ]);
+      })
     ]);
   }
 }
